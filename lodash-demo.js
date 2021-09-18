cea75d771c5bcf7e736b2677de23c85f31b35bb2
熊灵 <xiongling@sensorsdata.cn>
2021-09-18 17:17:57 +0800

    导入loadsh-4.17.15文件

---
+++ lodash-demo.js
@@ -0,0 +1,16 @@
+;(function(){
+
+    // 定义函数 lodash 
+    function lodash(value) {
+        if (isObjectLike(value) && !isArray(value) && !(value instanceof LazyWrapper)) {
+          if (value instanceof LodashWrapper) {
+            return value;
+          }
+          if (hasOwnProperty.call(value, '__wrapped__')) {
+            return wrapperClone(value);
+          }
+        }
+        return new LodashWrapper(value);
+    }
+
+}.call(this))
